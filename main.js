d855ecc762d248d8b0f91ad4c3f6b076e8af722d
Camilo9850 <71654326+Camilo9850@users.noreply.github.com>
2023-02-21 18:25:15 -0500

    1.2
    upload version  1.23

---
+++ main.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('#menu-icon');
+let menu = document.querySelector('.navbar');
+
+
+menu.ouclick =() =>{
+  navbar.classList.toggle('active');
+}
+
+window-}.onscroll =() =>{
+  navbar.classList.remove('active');
+}
